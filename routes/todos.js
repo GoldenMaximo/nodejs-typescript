923bb8625af0a09ea8aa47b5f674d6dc9972e8db
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-28 11:59:46 -0300

    feat: Add put and delete routes.

--- routes/todos.js
+++ routes/todos.js
@@ -1,34 +1,28 @@
-import { Router } from 'express';
-import { Todo } from '../models/todo';
-
-let todos: Array<Todo> = [];
-
-const router = Router();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = express_1.Router();
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
-
 router.post('/', (req, res, next) => {
-    const newTodo: Todo = {
+    const newTodo = {
         id: new Date().toISOString(),
         text: req.body.text
     };
-
     todos.push(newTodo);
     return res.status(200).json({ message: 'Todo added', newTodo, todos });
 });
-
 router.put('/todo/:todoId', (req, res, next) => {
     const tid = req.params.todoId;
     const todoIndex = todos.findIndex(e => e.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
         return res.status(200).json({ message: 'Updated successefully', todos });
     }
-    return res.status(404).json({ message: 'Todo not found '});
+    return res.status(404).json({ message: 'Todo not found ' });
 });
-
 router.delete('/todo/:todoId', (req, res, next) => {
     const tid = req.params.todoId;
     const todoIndex = todos.findIndex(e => e.id === tid);
@@ -36,7 +30,6 @@ router.delete('/todo/:todoId', (req, res, next) => {
         todos = todos.filter(e => e.id !== tid);
         return res.status(200).json({ message: 'Updated successefully', todos });
     }
-    return res.status(404).json({ message: 'not found '});
+    return res.status(404).json({ message: 'not found ' });
 });
-
-export default router;
+exports.default = router;
