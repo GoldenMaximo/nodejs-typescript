bb5f1f80cb41580af1011b73cecd0ccac2405ecf
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-28 12:17:03 -0300

    feat: Add type casting.

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string }
+
 let todos: Array<Todo> = [];
 
 const router = Router();
@@ -9,10 +12,12 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody;
+
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
 
     todos.push(newTodo);
@@ -20,17 +25,21 @@ router.post('/', (req, res, next) => {
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
+
     const todoIndex = todos.findIndex(e => e.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text }
         return res.status(200).json({ message: 'Updated successefully', todos });
     }
     return res.status(404).json({ message: 'Todo not found '});
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
     const todoIndex = todos.findIndex(e => e.id === tid);
     if (todoIndex >= 0) {
         todos = todos.filter(e => e.id !== tid);
